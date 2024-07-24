987b3d6556e69805d9de5823f7011b23101c483e
Sai-Keerthi9 <sheryala.sai-keerthi@capgemini.com>
2024-07-24 17:46:59 +0530

    updated TCs

--- src/shared/pages/accountPage.ts
+++ src/shared/pages/accountPage.ts
@@ -9,7 +9,7 @@ export class AccountPage {
 
     account(): Locator{return this.page.locator('div[id="TabBar-AccountTab"]');}
     accountSubMenu(): Locator{return this.page.locator('div[id="TabBar-AccountTab"] div[data-gw-click="toggleSubMenu"]')}
-    dropdown(): Locator{return this.page.locator('div[class*=gw-subMenu] #TabBar-AccountTab-AccountTab_NewAccount');}
+    dropdown(): Locator{return this.page.locator('div[class*="gw-subMenu gw-open"] div[class*="gw-action--inner"]');}
     errorMessage(): Locator{return this.page.locator('div[class*="gw-MessagesWidget--group-error"] div[class="gw-message"]');}
     missingFieldErrorMessage(): Locator{return this.page.locator('div .gw-message');}
     
@@ -21,22 +21,28 @@ export class AccountPage {
     postalCode():Locator {return this.page.locator('input[name*="PostalCode"][type="text"]');}
     searchButton():Locator {return this.page.locator('#NewAccount-NewAccountScreen-NewAccountSearchDV-SearchAndResetInputSet-SearchLinksInputSet-Search');}
     createAccount():Locator {return this.page.getByText('CNCreate New Account');}
-    accountType():Locator {return this.page.getByText('PePerson');}
+
+    homePhone():Locator{return this.page.locator('input[name*="HomePhone"]');}
+    workPhone(): Locator{return this.page.locator('input[name*="CreateAccountContactInputSet-Phone-GlobalPhoneInputSet"]');}
+    mobilePhone():Locator{return this.page.locator('input[name*="CellPhone"]');}
+    primaryPhone(): Locator{return this.page.locator('select[name*="PrimaryPhone"]')}
+    primaryEmail(): Locator{return this.page.locator('input[name*="EmailAddress1"]')}
     addressLine1():Locator {return this.page.locator('input[name="CreateAccount-CreateAccountScreen-CreateAccountDV-AddressInputSet-globalAddressContainer-GlobalAddressInputSet-AddressLine1"]');}
     addressType():Locator {return this.page.locator('select[name="CreateAccount-CreateAccountScreen-CreateAccountDV-AddressType"]');}
     organization():Locator {return this.page.locator('input[name="OrganizationSearchPopup-OrganizationSearchPopupScreen-OrganizationSearchDV-GlobalContactNameInputSet-Name"]');}
     orgSearch():Locator {return this.page.locator('#CreateAccount-CreateAccountScreen-CreateAccountDV-ProducerSelectionInputSet-Producer-SelectOrganization');}
     updateButton():Locator {return this.page.getByText('Update');}
+    
     producerCode(): Locator {return this.page.locator('select[name="CreateAccount-CreateAccountScreen-CreateAccountDV-ProducerSelectionInputSet-ProducerCode"]');}
     orgNameSearch() : Locator {return this.page.locator('#OrganizationSearchPopup-OrganizationSearchPopupScreen-OrganizationSearchDV-SearchAndResetInputSet-SearchLinksInputSet-Search');}
     orgSelect() : Locator {return this.page.getByText('Select');}
     accountHolderPostCreation() : Locator {return this.page.locator('#AccountFile_Summary-AccountSummaryDashboard-AccountDetailsDetailViewTile-AccountDetailsDetailViewTile_DV-AccountHolder_button')}
     detailsTitle(): Locator {return this.page.locator('div[aria-label="Details"] span[class="gw-TitleText"]')}
-    accountHolder(): Locator {return this.page.locator('div[id*="AccountHolder_Input"] div[data-gw-getset="text"]')}
+    accountHolder(): Locator {return this.page.locator('div[id*="AccountHolder_Input"] div[class*="gw-ActionValueWidget"]')}
     createdAccountName() : Locator {return this.page.locator('#NewAccount-NewAccountScreen-NewAccountSearchResultsLV-0-Name div .gw-value-readonly-wrapper')}
     
     async selectDropdown(value: string) {
-        await this.dropdown().getByLabel(value).click();
+        await this.dropdown().getByText(value).click();
     }
 
 }
--- src/tests/TC02_UserAccountCreation.spec.ts
+++ src/tests/TC02_UserAccountCreation.spec.ts
@@ -1,52 +1,113 @@
-import { test, expect } from '../shared/fixtures/base.ts';
 import accountDetails from '../shared/data/accountDetails.json';
 import loginData from '../shared/data/loginData.json';
+import { expect, test } from '../shared/fixtures/base.ts';
 
-
-test('Account Creation', async ({ loginPage, accountPage, page }) => {
-   
-    await page.goto(loginData.URL);
-    await accountPage.accountSubMenu().click();
-    await accountPage.dropdown().click();
-
-    await accountPage.firstName().click();
-    await accountPage.firstName().fill(accountDetails.firstName);
-    await accountPage.lastName().fill(accountDetails.lastName);
-
-    await accountPage.country().selectOption(accountDetails.country);
-    await page.waitForLoadState('networkidle');
-    await accountPage.city().fill(accountDetails.town);
-    await accountPage.postalCode().fill(accountDetails.postalCode);
-
-    Promise.all([
-        page.waitForLoadState('networkidle'),
-        accountPage.searchButton().click()
-    ])
-
-    await accountPage.createAccount().scrollIntoViewIfNeeded();
-    await accountPage.createAccount().click();
-    await accountPage.accountType().click();
-
-    await accountPage.addressLine1().click()
-    await accountPage.addressLine1().fill(accountDetails.addressLine1);
-    await accountPage.addressType().selectOption(accountDetails.addressType);
-    await accountPage.orgSearch().click();
-    await page.waitForLoadState('networkidle')
- 
-    await accountPage.organization().fill(accountDetails.organization);
-    await accountPage.orgNameSearch().click()
-    await accountPage.orgSelect().click()
-
-    await page.waitForLoadState('networkidle')
-    await accountPage.producerCode().selectOption(accountDetails.producerCode)
-    await accountPage.updateButton().click()
-
-    await page.waitForLoadState('networkidle')
-    await page.pause();
-    expect(await accountPage.accountHolderPostCreation().textContent()).toBe(accountDetails.firstName+" "+accountDetails.lastName)
+test.describe('Account Creation', ()=> {
+    test('Account Creation with full data', async ({ loginPage, accountPage, page }) => {
+        await page.goto(loginData.URL);
+        await accountPage.accountSubMenu().click();
+        await accountPage.selectDropdown('New Account');
     
+        await accountPage.firstName().click();
+        await accountPage.firstName().fill(accountDetails.firstName);
+        await accountPage.lastName().fill(accountDetails.lastName);
+    
+        await accountPage.country().selectOption(accountDetails.country);
+        await page.waitForLoadState('networkidle');
+        await accountPage.city().fill(accountDetails.town);
+        await accountPage.postalCode().fill(accountDetails.postalCode);
+    
+        Promise.all([
+            page.waitForLoadState('networkidle'),
+            accountPage.searchButton().click()
+        ])
+    
+        await accountPage.createAccount().scrollIntoViewIfNeeded();
+        await accountPage.createAccount().click();
+        await accountPage.selectDropdown(accountDetails.accountType);
+    
+        await accountPage.homePhone().fill(accountDetails.homePhone);
+        await accountPage.workPhone().fill(accountDetails.workPhone);
+        await accountPage.mobilePhone().fill(accountDetails.mobilePhone); 
+        await accountPage.primaryPhone().selectOption(accountDetails.primaryPhone);
+        await accountPage.primaryEmail().fill(accountDetails.primaryEmail);
+
+        await accountPage.addressLine1().click()
+        await accountPage.addressLine1().fill(accountDetails.addressLine1);
+        await accountPage.addressType().selectOption(accountDetails.addressType);
+        await accountPage.orgSearch().click();
+        await page.waitForLoadState('networkidle')
+     
+        await accountPage.organization().fill(accountDetails.organization);
+        await accountPage.orgNameSearch().click()
+        await accountPage.orgSelect().click()
+    
+        await page.waitForLoadState('networkidle')
+        await accountPage.producerCode().selectOption(accountDetails.producerCode)
+        await accountPage.updateButton().click()
+    
+        await page.waitForLoadState('networkidle')
+        await expect(await accountPage.accountHolderPostCreation()).toHaveText(accountDetails.firstName+" "+accountDetails.lastName)
+        
+    })
+    test('Mandate Field Filled Check', async ({ loginPage, accountPage, page }) => {
+   
+        await page.goto(loginData.URL);
+        await accountPage.accountSubMenu().click();
+        await accountPage.selectDropdown('New Account');
+    
+        await accountPage.firstName().click();
+        await accountPage.firstName().fill(accountDetails.firstName);
+        await accountPage.lastName().fill(accountDetails.lastName);
+    
+        Promise.all([
+            page.waitForLoadState('networkidle'),
+            accountPage.searchButton().click()
+        ])
+    
+        await accountPage.createAccount().scrollIntoViewIfNeeded();
+        await accountPage.createAccount().click();
+        await accountPage.selectDropdown(accountDetails.accountType);
+        await accountPage.updateButton().click();
+        
+        await expect(accountPage.missingFieldErrorMessage().first()).toHaveText(accountDetails.errorCodeMissingFilel)
+    
+    })
+    test('Name Of The Account Holder Already Exist', async ({ loginPage, accountPage, page }) => {
+   
+        await page.goto(loginData.URL);
+        await accountPage.accountSubMenu().click();
+        await accountPage.selectDropdown('New Account');
+    
+        await accountPage.firstName().click()
+        await accountPage.firstName().fill(accountDetails.firstName);
+        await accountPage.lastName().fill(accountDetails.lastName);
+        
+        Promise.all([
+            page.waitForLoadState('load'),
+            accountPage.searchButton().click()
+        ])
+    
+        await expect(accountPage.createdAccountName()).toHaveText(accountDetails.firstName+" "+accountDetails.lastName)
+    })
+    test('Test to Enter Both Person and Company Details', async ({ loginPage, accountPage, page }) => {
+       
+        await page.goto(loginData.URL);
+        await accountPage.accountSubMenu().click();
+        await accountPage.selectDropdown('New Account');
+    
+        await accountPage.company().click()
+        await accountPage.company().fill(accountDetails.negTC_both.company);
+        await accountPage.firstName().fill(accountDetails.firstName);
+        await accountPage.lastName().fill(accountDetails.lastName);
+    
+       Promise.all([
+            page.waitForLoadState('load'),
+            accountPage.searchButton().click()
+        ])
+        
+        await expect(await accountPage.errorMessage()).toHaveText(accountDetails.negTC_both.errorMsg);
+    
+    })
 })
-
-
-
 
